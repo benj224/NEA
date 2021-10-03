2d65ce2d60529724a310292cd4ff527f6f010fc6
benj224 <52797746+benj224@users.noreply.github.com>
2021-10-03 20:33:55 +0100

    fixed and compiled

--- v01/lib/main.dart
+++ v01/lib/main.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:file_picker/file_picker.dart";
 
+import 'login.dart';
+
 void main(){
   runApp(MyApp());
 }
@@ -31,10 +33,21 @@ class MyHomePage extends StatelessWidget{
           child: Text("List of TX"),
         ),
       ],),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          selectFile();
+
+      floatingActionButton: Row(
+        children: [FloatingActionButton(
+          onPressed: (){
+            selectFile();
         },
+      ),
+        const SizedBox(
+          width: 200,
+        ),
+        FloatingActionButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => LogIn(key: UniqueKey(), title: "test title",)));
+          },
+        )]
       ),
     );
   }
@@ -48,7 +61,6 @@ void selectFile() async{
   );
   if (result != null){
     PlatformFile file = result.files.first;
-
-
   }
 }
+
