70809f0ee6677eeb7f64df3186a040e429abd4af
benj224 <52797746+benj224@users.noreply.github.com>
2021-10-05 20:17:00 +0100

    got a scroll widget working now just to break it again tomorow

--- v01/lib/main.dart
+++ v01/lib/main.dart
@@ -20,96 +20,67 @@ class MyApp extends StatelessWidget{
 class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+
+    final List<Widget> packDisplays = [
+      PackDisplay(),
+      PackDisplay(),
+      PackDisplay(),
+      PackDisplay(),
+      PackDisplay(),
+      PackDisplay(),
+    ];
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
 
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            top: 0,
-            child: SingleChildScrollView(
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 10,
-                    top: 10,
-                    child: Material(
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Container(
-                        width: 150,
-                        height: 200,
-                        color: Colors.red,
-                        child: Stack(
-                          children: <Widget>[
-                            Align(
-                              alignment: FractionalOffset(0.5, 0.1),
-                              child:Text("Test") ,
-                            )
-                          ],
-                        ),
-                      )
-                    ),
-                  ),
-                  Positioned(
-                    left: 170,
-                    top: 220,
-                    child: Material(
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Container(
-                          width: 150,
-                          height: 200,
-                          color: Colors.green,
-                          child: Stack(
-                            children: <Widget>[
-                              Align(
-                                alignment: FractionalOffset(0.5, 0.1),
-                                child:Text("Test2") ,
-                              )
-                            ],
-                          ),
-                        )
-                    ),
-                  ),
-                ],
-              )
-            )
-          )
-        ],
-      ),
-
-
-
-
-      floatingActionButton: Row(
-        children: [
-          Align(
-            child: FloatingActionButton(
-              onPressed: (){
-                selectFile();
-              },
-            ),
-          alignment: Alignment.bottomLeft
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              IceCreamCard(flavorColor: Colors.redAccent, flavor: 'Strawberry'),
+              IceCreamCard(flavorColor: Colors.brown, flavor: 'Chocolate'),
+              IceCreamCard(flavorColor: Colors.blueAccent, flavor: 'Blueberry'),
+              IceCreamCard(flavorColor: Colors.amberAccent, flavor: 'Lemon'),
+              IceCreamCard(flavorColor: Colors.greenAccent, flavor: 'Mint'),
+            ],
           ),
-        const SizedBox(
-          width: 200,
-        ),Align(
-            child: FloatingActionButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LogIn(key: UniqueKey(), title: "test title",)));
-              },
+        ),
+      ),
+    );
+  }
+}
+
+
+class PackDisplay extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SizedBox(
+        width: 100,
+        height: 200,
+        child: Material(
+            elevation: 5,
+            color: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-            alignment: Alignment.bottomRight,
-          )
-        ]
+            child: Container(
+              width: 100,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: FractionalOffset(0.5, 0.1),
+                    child:Text("Test") ,
+                  )
+                ],
+              ),
+            )
+        ),
       ),
     );
   }
@@ -123,6 +94,60 @@ void selectFile() async{
   );
   if (result != null){
     PlatformFile file = result.files.first;
+  }
+}
+
+
+class IceCreamCard extends StatelessWidget {
+  const IceCreamCard({
+    this.flavorColor = Colors.redAccent,
+    this.flavor = 'Flavor Name',
+  });
+
+  final Color flavorColor;
+  final String flavor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xffeeeeee), width: 2.0),
+        color: Colors.white38,
+        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white10,
+            blurRadius: 4,
+            spreadRadius: 2,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      margin: EdgeInsets.all(8),
+      height: 200,
+      width: 200,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Icon(
+                Icons.icecream,
+                size: 100.0,
+                color: flavorColor,
+              )),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            flavor,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+                color: flavorColor),
+          ),
+        ],
+      ),
+    );
   }
 }
 
